784cecbe9c57624ffdfa4687d6bf093d279928a9
Evelyn <evelyn.depiala@gmail.com>
2021-03-01 00:52:36 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/global.css'
+
+
+import { useState } from 'react'
+import { CountdownProvider } from '../contexts/CountdownContext'
+
+
+
+
+
+function MyApp({ Component, pageProps }) {
+
+  
+  return (
+    
+            <Component {...pageProps} />
+    
+  )
+}
+
+export default MyApp
